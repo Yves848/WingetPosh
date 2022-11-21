ac0235de7bb9d34f31aff86117d125f87a6a0d90
yves848 <yves.godart@gmail.com>
2022-11-21 11:59:44 +0100

    Cleaning

--- experiment.ps1
+++
@@ -1,3 +0,0 @@
-NugetPackageRoot  = $tempModulePath
-$tempModulePath = Microsoft.PowerShell.Management\Join-Path -Path $script:TempPath `
-            -ChildPath "$(Microsoft.PowerShell.Utility\Get-Random)\$moduleName"
