9e1a8d5c7252892ca628710638927efc7cbb0dd3
yves848 <yves.godart@gmail.com>
2022-12-02 15:50:26 +0100

    remove apikey

--- publish.ps1
+++ publish.ps1
@@ -1,2 +1,3 @@
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+$pskey = Get-Content .\apikey.txt
 Publish-Module -Name 'wingetposh' -NuGetApiKey $pskey -Verbose -RequiredVersion '0.5.2' 
