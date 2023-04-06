3b83a384c07d5a57580642ee61a54a8fc8a7a17b
yves848 <yves.godart@gmail.com>
2023-04-06 12:12:33 +0200

    0.7.5

--- publish.ps1
+++ publish.ps1
@@ -1,3 +1,3 @@
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 $pskey = Get-Content ~\.config\.private\apikey.txt
-Publish-Module -Name 'wingetposh' -NuGetApiKey $pskey -Verbose -RequiredVersion '0.7.4' 
+Publish-Module -Name 'wingetposh' -NuGetApiKey $pskey -Verbose -RequiredVersion '0.7.5' 
