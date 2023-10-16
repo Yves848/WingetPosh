db323586755940c36674e6191a488c6c2c55add7
yves848 <yves.godart@gmail.com>
2023-10-16 10:28:07 +0200

    extract tests from main script

--- SandBox/wingetposh.ps1
+++ SandBox/wingetposh.ps1
@@ -1,7 +1,4 @@
-﻿param (
-  [ValidateSet("Show-WGList", "Install-WGPackage", "Search-WGPackage", "Get-ScoopStatus", "Test-Scoop")]$func
-)
-$include = [System.IO.Path]::GetDirectoryName($myInvocation.MyCommand.Definition) 
+﻿$include = [System.IO.Path]::GetDirectoryName($myInvocation.MyCommand.Definition) 
 
 . "$include\visuals.ps1"
 
@@ -1247,37 +1244,4 @@ function Set-WingetposhConfig {
 
 function Reset-WingetposhConfig {
   '{ "UseNerdFont" : false, "SilentInstall": false, "AcceptPackageAgreements" : true, "AcceptSourceAgreements" : true,"Force": false, "IncludeScoop": false }' | Out-File -FilePath ~/.config/.wingetposh/config.json -Force | Out-Null
-}
-# CUT HERE #
-
-switch ($func) {
-  "Get-ScoopStatus" { Get-ScoopStatus }
-  "Show-WGList" { Show-WGList }
-  "Install-WGPackage" { Install-WGPackage }
-  "Search-WGPackage" { Search-WGPackage }
-  "Test-Scoop" {
-    Get-WingetposhConfig
-    $script:config.IncludeScoop 
-    Test-Path -Path "$env:HOMEDRIVE$env:HOMEPATH\Scoop\"
-  }
-  Default {
-    #Search-WGPackage -package git
-    #Get-WGPackage
-    #Search-WGPackage -interactive -search git
-    Install-WGPackage -package neofetch
-    #Get-WGPackage -interactive -update
-    #Get-WGUpdatables
-    #Get-WGList -source $args
-    #Show-WGList
-    #Update-WGPackage -apply
-    #Search-WGPackage -source $args -interactive -allowSearch
-    #Uninstall-WGPackage -source winget -apply
-    #Get-WGSources
-    #Set-WingetposhConfig -param UseNerdFont -value $args
-    #Install-WGPackage 
-    #Get-WGPVersion -param All
-    #Get-ScoopStatus
-    #Get-ScoopBuckets
-    #Reset-WingetposhConfig
-  }
 }
